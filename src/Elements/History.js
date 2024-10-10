776a8bea61e749bfbbdb465815a2cba5baa675c2
jainaMarvadi <160025316+jainaMarvadi@users.noreply.github.com>
2024-10-11 00:01:53 +0530

    Update History.js

--- src/Elements/History.js
+++ src/Elements/History.js
@@ -3,6 +3,7 @@ const History=()=>{
         <>
         <div className="container">
             <h2>Your Shopping Adventure </h2>
+            <p>You Didn't Buy Anything in Past!</p>
         </div>
         </>
     )
